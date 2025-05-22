3cd40d20411863919c7959cce7ddf669ca77fe0b
Alex <alexringme@gmail.com>
2025-05-22 17:54:12 -0400

    file restructure

--- api/src/catergorization.rs
+++ api/src/catergorization.rs
@@ -8,7 +8,7 @@ use reqwest::Client;
 
 use lazy_static::lazy_static;
 
-const API_ENDPOINT: &str = "https://app.fina.money/api/resource/categorize";
+const API_ENDPOINT: &'static str = "https://app.fina.money/api/resource/categorize";
 
 lazy_static! {
     static ref CLIENT: Client = Client::new();
@@ -17,7 +17,7 @@ lazy_static! {
 pub async fn catergorize_transactions(transaction: &[Transaction]) -> Result<Vec<String>, Error> {
     let mut headers = HeaderMap::new();
     headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
-    headers.insert("x-api-key", "fina-api-test".parse().unwrap());
+    headers.insert("x-api-key", "fina-api-test".parse().unwrap()); //api key is free and open to everyone
     headers.insert("x-partner-id", "<client-parter-id>".parse().unwrap());
     headers.insert("x-api-model", "v3".parse().unwrap());
     headers.insert("x-api-mapping", "true".parse().unwrap());
@@ -30,7 +30,7 @@ pub async fn catergorize_transactions(transaction: &[Transaction]) -> Result<Vec
     let mut all_responses = vec![];
     for chunk in data.chunks(100) {
         let response = CLIENT
-            .post(API_ENDPOINT)
+            .post(API_ENDPOINT.to_string())
             .headers(headers.clone())
             .json(&chunk)
             .send()
